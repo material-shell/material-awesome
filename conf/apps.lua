6f2da48a538bce4f0d57d6786d8437a73a2f4726
PapyElGringo <adrien.peslerbe@bemyeye.com>
2019-02-11 12:53:36 +0100

    unify lock and add it to the apps conf

--- conf/apps.lua
+++ conf/apps.lua
@@ -1,7 +1,8 @@
 local filesystem = require('gears.filesystem')
 
 return {
-    terminal = 'alacritty',
-    editor = 'code',
-    rofi = 'rofi -show drun -theme ' .. filesystem.get_configuration_dir() .. '/conf/rofi.rasi'
+  terminal = 'alacritty',
+  editor = 'code',
+  rofi = 'rofi -show drun -theme ' .. filesystem.get_configuration_dir() .. '/conf/rofi.rasi',
+  lock = 'i3lock-fancy-rapid 5 3 -k --timecolor=ffffffff --datecolor=ffffffff'
 }
