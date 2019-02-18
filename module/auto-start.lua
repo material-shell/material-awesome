77a5e36e708b1112c86c4db53aa128e811886117
PapyElGringo <adrienpeslerbe@gmail.com>
2019-02-18 09:57:58 +0100

    rollback auto-start to previous custom cmd

--- module/auto-start.lua
+++ module/auto-start.lua
@@ -1,13 +1,22 @@
 local awful = require('awful')
 local filesystem = require('gears.filesystem')
 
-awful.spawn.single_instance({'blueberry-tray'}) -- Bluetooth tray icon
-awful.spawn.single_instance({'xfce4-power-manager'}) -- Power manager
-awful.spawn.single_instance('compton --config ' .. filesystem.get_configuration_dir() .. '/conf/compton.conf')
+local function run_once(cmd_arr)
+  for _, cmd in ipairs(cmd_arr) do
+    local findme = cmd
+    local firstspace = cmd:find(' ')
+    if firstspace then
+      findme = cmd:sub(0, firstspace - 1)
+    end
+    awful.spawn.with_shell(string.format('pgrep -u $USER -x %s > /dev/null || (%s)', findme, cmd))
+  end
+end
+
+run_once({'blueberry-tray'}) -- Bluetooth tray icon
+run_once({'xfce4-power-manager'}) -- Power manager
+run_once('compton --config ' .. filesystem.get_configuration_dir() .. '/conf/compton.conf')
 -- To allow gnome tools to ask authentication like pamac
-awful.spawn.single_instance(
-  {
-    '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)'
-  }
+run_once(
+  '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)'
 )
 -- run_once({'pamac-tray'})
