35dcf175e286c70f7571ce9f94595e47d94d3667
PapyElGringo <adrienpeslerbe@gmail.com>
2019-02-21 12:30:33 +0100

    rename theme and add demo.gif

--- theme/init.lua
+++ theme/init.lua
@@ -1,7 +1,7 @@
 local gtable = require('gears.table')
 local default_theme = require('theme.default-theme')
 -- PICK THEME HERE
-local theme = require('theme.my-theme')
+local theme = require('theme.PapyElGringo-theme')
 
 local final_theme = {}
 gtable.crush(final_theme, default_theme.theme)
